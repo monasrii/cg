725868e281789511cc5505fa3c481e59cf7ad895
mona-12-milan <76400168+mona-12-milan@users.noreply.github.com>
2023-03-30 22:49:42 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,78 +1,152 @@
+/*
+ * GLUT Shapes Demo
+ *
+ * Written by Nigel Stewart November 2003
+ *
+ * This program is test harness for the sphere, cone
+ * and torus shapes in GLUT.
+ *
+ * Spinning wireframe and smooth shaded shapes are
+ * displayed until the ESC or q key is pressed.  The
+ * number of geometry stacks and slices can be adjusted
+ * using the + and - keys.
+ */
+
+#include<windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
 
-// Window size
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
+#include <stdlib.h>
 
-// Rotation angles
-float rotationX = 0.0f;
-float rotationY = 0.0f;
 
-// Initialization function
-void init() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glEnable(GL_DEPTH_TEST);
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+void init(){
+     glClearColor(1,1,1,0);
+     glMatrixMode(GL_PROJECTION);
+     glLoadIdentity();
+     gluOrtho2D(-320,320,-320,320);
+
+
 }
 
-// Display function
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0.0f, 0.0f, -5.0f);
-    glRotatef(rotationX, 1.0f, 0.0f, 0.0f);
-    glRotatef(rotationY, 0.0f, 1.0f, 0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glutWireSphere(1.0f, 32, 32);
-    glutSwapBuffers();
-}
+class Point{
 
-// Reshape function
-void reshape(int width, int height) {
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0f, (float)width / height, 0.1f, 100.0f);
-}
+public :
 
-// Keyboard function
-void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-        case 'q':
-        case 'Q':
-        case 27:  // Escape key
-            exit(0);
-            break;
+    int x,y;
+    Point(int x,int y){
+
+    this->x = x;
+    this->y = y;
     }
-}
 
-// Mouse function
-void mouse(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        rotationX += 5.0f;
+};
+
+void drawCurve(vector<Point> controlPoints)
+{
+    vector<Point> curve;
+
+    for(float t = 0.1 ; t < 1 ; t += 0.1 )
+    {
+        float x = (1-t)*(1-t)*(1-t)*controlPoints[0].x + 3*t*(1-t)*(1-t)*controlPoints[1].x + 3*t*t*(1-t)*controlPoints[2].x + t*t*t*controlPoints[3].x;
+        float y = (1-t)*(1-t)*(1-t)*controlPoints[0].y + 3*t*(1-t)*(1-t)*controlPoints[1].y + 3*t*t*(1-t)*controlPoints[2].y + t*t*t*controlPoints[3].y;
+
+        curve.push_back(Point((int)x,(int)y));
     }
-    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-        rotationY += 5.0f;
+
+    glBegin(GL_LINE_STRIP);
+    for(int i = 0 ;i< curve.size() ; i++)
+        glVertex2d(curve[i].x,curve[i].y);
+    glEnd();
+    glFlush();
+
+
+}
+vector<Point> controlPoints;
+
+void mouseHandler(int button, int state, int mouse_x, int mouse_y)
+{
+    static int count = 0;
+
+    glColor3d(1,0,0);
+
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+
+
+        int x = mouse_x - 320;
+        int y = 640 - mouse_y - 320;
+
+        if(count%4 <= 2 )
+        {
+
+            controlPoints.push_back(Point(x,y));
+            glBegin(GL_POINTS);
+            glVertex2d(x,y);
+            glEnd();
+            glFlush();
+            count ++;
+
+        }
+
+        else if(count%4 == 3)
+        {
+            controlPoints.push_back(Point(x,y));
+            glBegin(GL_POINTS);
+            glVertex2d(x,y);
+            glEnd();
+            glFlush();
+            count++;
+
+            drawCurve(controlPoints);
+            controlPoints.clear();
+
+        }
+
+        glFlush();
+
     }
+
+    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN )
+    {
+
+
+    }
+
+
+}
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+      glClearColor(0,0,0,1);
+
 }
 
-// Idle function
-void idle() {
-    glutPostRedisplay();
-}
-
-// Main function
-int main(int argc, char** argv) {
+int main(int argc, char *argv[])
+{
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-    glutCreateWindow("Sphere");
+    glutInitWindowSize(640,640);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+
+    glutCreateWindow("curve");
+
+
     init();
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutMouseFunc(mouse);
-    glutIdleFunc(idle);
+    glutMouseFunc(mouseHandler);
+
+
+    glClearColor(1,1,1,1);
+
     glutMainLoop();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
